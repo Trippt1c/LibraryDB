beb6f71bb00825ffa35c56434331c93de8f8fb53
Nabee <Nabee@10.177.184.182>
2023-11-28 17:29:13 -0600

    search UI

--- src/main/java/BookPage.java
+++ src/main/java/BookPage.java
@@ -3,35 +3,31 @@ import javax.swing.*;
 import java.awt.event.*;
 public class BookPage {
 	//get required data
-	String bookTitle = "";
-	String bookAuthor = "";
-	String bookisbn = "";
-	int booksInCart = 0;
-	boolean isCheckedOut = false;
+	private String bookTitle = "";
+	private String bookAuthor = "";
+	private String bookisbn = "";
+	private int booksInCart = 0;
+	private boolean isCheckedOut = false;
 			
-	public BookPage() {
-		//get required data
-		//TODO replace test data with method to obtain real data from database
-		bookTitle = "Test Book";
-		bookAuthor = "AuthorName";
-		bookisbn = "0123456789";
+	public BookPage(Book book) {
+		//initialize data
+		bookTitle = book.getTitle();
+		bookAuthor = book.getAuthor();
+		bookisbn = book.getisbn();
 		booksInCart = 0;
-		isCheckedOut = false;
+		isCheckedOut = book.getStatus();
 	}
 	
 	public void display(JPanel displayBox) {
 		
 		//JLabels used to display book info
 		JLabel title = new JLabel(bookTitle);
-		title.setBounds(20, 30, 100, 5000);
 		displayBox.add(title);
 		
 		JLabel author = new JLabel("Author: "+bookAuthor);
-		author.setBounds(20, 70, 100, 40);
 		displayBox.add(author);
 		
 		JLabel isbn = new JLabel("ISBN: "+bookisbn);
-		isbn.setBounds(20, 120, 100, 40);
 		displayBox.add(isbn);
 		
 		String statusString = "";
@@ -42,12 +38,10 @@ public class BookPage {
 			statusString = "Checked out";
 		}
 		JLabel status = new JLabel(statusString);
-		status.setBounds(20,140,100,40);
 		displayBox.add(status);
 		
 		//rent button will create a new window notifying user of success/failure
 		JButton rent = new JButton("Borrow this book");
-		rent.setBounds(10, 65, 100, 40);
 		displayBox.add(rent);		
 		rent.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -74,9 +68,9 @@ public class BookPage {
 		});
 		
 		//This button could link to the info from BOOK_AUTHORS table
-		JButton authorPage = new JButton("More from this author");
-		authorPage.setBounds(10, 105, 200, 20);
-		displayBox.add(authorPage);
+		//JButton authorPage = new JButton("More from this author");
+		//authorPage.setBounds(10, 105, 200, 20);
+		//displayBox.add(authorPage);
 		
 	}
 }
