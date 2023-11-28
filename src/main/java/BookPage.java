a7545913d773a8d6ec7420f57523dcbf1b93845e
Nabee <Nabee@10.177.184.182>
2023-11-28 17:14:03 -0600

    Revert "multi-page search UI"
    This reverts commit b298ca9c19bb4c14805b5a808227859892560d3b.

--- src/main/java/BookPage.java
+++ src/main/java/BookPage.java
@@ -3,31 +3,35 @@ import javax.swing.*;
 import java.awt.event.*;
 public class BookPage {
 	//get required data
-	private String bookTitle = "";
-	private String bookAuthor = "";
-	private String bookisbn = "";
-	private int booksInCart = 0;
-	private boolean isCheckedOut = false;
+	String bookTitle = "";
+	String bookAuthor = "";
+	String bookisbn = "";
+	int booksInCart = 0;
+	boolean isCheckedOut = false;
 			
-	public BookPage(Book book) {
-		//initialize data
-		bookTitle = book.getTitle();
-		bookAuthor = book.getAuthor();
-		bookisbn = book.getisbn();
+	public BookPage() {
+		//get required data
+		//TODO replace test data with method to obtain real data from database
+		bookTitle = "Test Book";
+		bookAuthor = "AuthorName";
+		bookisbn = "0123456789";
 		booksInCart = 0;
-		isCheckedOut = book.getStatus();
+		isCheckedOut = false;
 	}
 	
 	public void display(JPanel displayBox) {
 		
 		//JLabels used to display book info
 		JLabel title = new JLabel(bookTitle);
+		title.setBounds(20, 30, 100, 5000);
 		displayBox.add(title);
 		
 		JLabel author = new JLabel("Author: "+bookAuthor);
+		author.setBounds(20, 70, 100, 40);
 		displayBox.add(author);
 		
 		JLabel isbn = new JLabel("ISBN: "+bookisbn);
+		isbn.setBounds(20, 120, 100, 40);
 		displayBox.add(isbn);
 		
 		String statusString = "";
@@ -38,10 +42,12 @@ public class BookPage {
 			statusString = "Checked out";
 		}
 		JLabel status = new JLabel(statusString);
+		status.setBounds(20,140,100,40);
 		displayBox.add(status);
 		
 		//rent button will create a new window notifying user of success/failure
 		JButton rent = new JButton("Borrow this book");
+		rent.setBounds(10, 65, 100, 40);
 		displayBox.add(rent);		
 		rent.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -68,9 +74,9 @@ public class BookPage {
 		});
 		
 		//This button could link to the info from BOOK_AUTHORS table
-		//JButton authorPage = new JButton("More from this author");
-		//authorPage.setBounds(10, 105, 200, 20);
-		//displayBox.add(authorPage);
+		JButton authorPage = new JButton("More from this author");
+		authorPage.setBounds(10, 105, 200, 20);
+		displayBox.add(authorPage);
 		
 	}
 }
