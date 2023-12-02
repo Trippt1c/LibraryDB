a62a30e7950583f40dae3f10b22138ad2a0215ab
Trippt1c <dylantrang1234@gmail.com>
2023-12-01 22:31:37 -0600

    fixed searches

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,51 +1,44 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import javax.swing.*;
 
 public class Sample
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws SQLException
     {
-        Connection connection = null;
-        try
+        QueryHandler handler = new QueryHandler();
+        ResultSet matchingTitles = handler.query("SELECT * FROM BOOK WHERE Title LIKE '%Classical%'");
+        while (matchingTitles.next())
         {
-            // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            //.out.println("matching titles");
+            //printResultSet(matchingTitles);
 
-            statement.executeUpdate("drop table if exists person");
-            statement.executeUpdate("create table person (id integer, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
-            ResultSet rs = statement.executeQuery("select * from person");
-            while(rs.next())
+            String title = matchingTitles.getString("Title");
+            String isbn = matchingTitles.getString("Isbn");
+            System.out.println("isbn: " + isbn);
+            ResultSet authorids = handler.query("SELECT * FROM BOOK_AUTHORS WHERE Isbn LIKE '%" + isbn + "%'");
+            //System.out.println("author ids");
+            //printResultSet(authorids);
+            while (authorids.next())
             {
-                // read the result set
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
+                String authorid = authorids.getString("Author_id");
+                String authorName = authorids.getString("Name");
+                System.out.println("author id: " + authorid);
+                System.out.println("author name: " + authorName);
+                ResultSet authorNames = handler.query("SELECT * FROM AUTHORS WHERE Author_id LIKE '%" + authorid + "%'");
+                System.out.println("author names");
+                printResultSet(authorNames);
             }
+
         }
-        catch(SQLException e)
+    }
+    public static void printResultSet(ResultSet rs) throws SQLException
+    {
+        int col = rs.getMetaData().getColumnCount();
+        while (rs.next())
         {
-            // if the error message is "out of memory",
-            // it probably means no database file is found
-            System.err.println(e.getMessage());
-        }
-        finally
-        {
-            try
+            for (int i = 1; i <= col; i++)
             {
-                if(connection != null)
-                    connection.close();
-            }
-            catch(SQLException e)
-            {
-                // connection close failed.
-                System.err.println(e.getMessage());
+                System.out.println(rs.getString(i) + " ");
             }
         }
     }
